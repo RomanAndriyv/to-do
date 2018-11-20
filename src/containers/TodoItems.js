188da48104a3d0384d974759ae0f532024d4456f
RomanAndriyv <roman.andriyv@gmail.com>
2018-11-20 15:23:31 +0200

    Initial commit

---
+++ src/containers/TodoItems.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class TodoItems extends Component {
+    constructor(props) {
+        super(props);
+
+        this.createTasks = this.createTasks.bind(this);
+    }
+
+    createTasks(item) {
+        return <li onClick={() => this.delete(item.key)}
+                    key={item.key}>{item.text}</li>
+    }
+
+    delete(key) {
+        this.props.delete(key);
+        console.log(this);
+        console.log(this.props);
+    }
+
+    render() {
+        let todoEntries = this.props.entries;
+        let listItems;
+        if (todoEntries) listItems = todoEntries.map(this.createTasks);
+
+        return (
+            <ul className="theList">
+                {listItems}
+            </ul>
+        );
+    }
+}
+
+export default TodoItems;
