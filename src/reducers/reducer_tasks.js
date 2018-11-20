08cfedee855d4b5bf47b40ea4c23426bde79b3af
RomanAndriyv <roman.andriyv@gmail.com>
2018-11-20 17:24:51 +0200

     Added Redux

--- src/reducers/reducer_tasks.js
+++ src/reducers/reducer_tasks.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        {text: "1a", key: 154271566281},
-        {text: "2b", key: 154271566282},
-        {text: "3c", key: 154271566283},
-        {text: "4d", key: 154271566284}
+        { text: "1a", key: 154271566281 },
+        { text: "2b", key: 154271566282 },
+        { text: "3c", key: 154271566283 },
+        { text: "4d", key: 154271566284 }
     ]
 }
