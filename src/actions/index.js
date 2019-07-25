22668da3e6d8273d4bf2886ecdf85278f884da6f
RomanAndriiv <roman.andriyv@gmail.com>
2019-07-25 10:01:38 +0300

    Improved saga effect

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,10 @@ export const ADD_TASK = 'ADD_TASK';
 export const ADD_TASK_ASYNC = 'ADD_TASK_ASYNC';
 export const DELETE_TASK = 'DELETE_TASK';
 
+let nextId = 0;
 export const addTask = (text) => {
     return {
+        id: nextId++,
         text,
         type: ADD_TASK
     }
--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -2,11 +2,9 @@ import { ADD_TASK } from "../actions";
 import { ADD_TASK_ASYNC } from "../actions";
 import { delay, takeLatest, put } from 'redux-saga/effects';
 
-
-let nextId = 0;
-function* addTaskAsync() {
-    yield delay(4000);
-    yield put({type: ADD_TASK_ASYNC, id: nextId++, text: 'params.text'});
+function* addTaskAsync(action) {
+    yield delay(2000);
+    yield put({type: ADD_TASK_ASYNC, id: action.id, text: action.text});
 }
 
 export function* watchTaskAdd() {
